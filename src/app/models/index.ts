01eb7596c6e6dc73bfd94bbf9ed1ba8127921b9d
Kawtar <Kawtar.shaimi8@gmail.com>
2026-01-19 16:45:50 +0100

    feat: export models from index

---
+++ src/app/models/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Barrel export pour tous les modèles
+ * Permet d'importer facilement tous les modèles depuis un seul endroit
+ */
+export * from './track.model';
+export * from './audio-player-state.model';
+export * from './category.enum';
+export * from './player-state.enum';
+export * from './loading-state.enum';
