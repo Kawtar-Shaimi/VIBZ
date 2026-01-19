90ee7962de89fc5803ed6709b0c4d8e4a5be6375
Kawtar <Kawtar.shaimi8@gmail.com>
2026-01-19 16:04:25 +0100

    feat: add category enums

---
+++ src/app/models/category.enum.ts
@@ -0,0 +1,19 @@
+/**
+ * Énumération des catégories musicales disponibles
+ * Utilisé pour classifier les pistes dans l'application
+ */
+export enum MusicCategory {
+    POP = 'Pop',
+    ROCK = 'Rock',
+    RAP = 'Rap',
+    JAZZ = 'Jazz',
+    CLASSICAL = 'Classical',
+    ELECTRONIC = 'Electronic',
+    HIPHOP = 'Hip-Hop',
+    RNB = 'R&B',
+    COUNTRY = 'Country',
+    BLUES = 'Blues',
+    METAL = 'Metal',
+    INDIE = 'Indie',
+    OTHER = 'Other'
+}
